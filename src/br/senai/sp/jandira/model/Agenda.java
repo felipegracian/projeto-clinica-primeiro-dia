4135cd82aa839183614b9d30952f3fef9381cefa
Felipe Graciano <22282790@cfp127aluno.local>
2022-09-19 16:57:15 -0300

    projeto clinica segundo commit

---
+++ src/br/senai/sp/jandira/model/Agenda.java
@@ -0,0 +1,54 @@
+package br.senai.sp.jandira.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Agenda {
+
+	private LocalDate data;
+	private LocalTime hora;
+	private Paciente paciente;
+	private Medico medico;
+	private Especialidade especialidade;
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+}
---
+++ src/br/senai/sp/jandira/model/Endereco.java
@@ -0,0 +1,71 @@
+package br.senai.sp.jandira.model;
+
+public class Endereco {
+	
+	private String logradouro;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String complemento;
+	private Estados estado;
+	private String cep;
+	
+	//Métodos de acesso
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Estados getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(Estados estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+}
